f2f99ae637ddbdc62ca21a659b7e3937b9623008
Lars Knickrehm <mail@lars-sh.de>
2013-01-25 04:55:01 +0100

    Restructured creator.sh

--- creator.sh
+++ creator.sh
@@ -1,86 +1,267 @@
 #!/bin/bash
-cd=$(realpath .)
-cd $(dirname "$0")
+#
+# Copyright © since 2013 Lars Knickrehm, mail@lars-sh.de
+#
 
-oneye_path=$(realpath '../../www/oneye')
-installer_path=$(realpath '../../www/installer')
+#
+# HELP
+#
 
-browser_dir='browser'
-iphone_dir='iphone'
-mobile_dir='mobile'
-oneyexxxxxxxxxx_dir='eyeOS'
+##
+# HELP
+#
+# Prints the help to stdout
+function creator_help() {
+	cat << EOF
+creator.sh (-h|--help)
+  Print this help and exit
 
-files_path="$installer_path/installer/files"
-modules_path="$files_path/modules.xml"
+creator.sh ONEYE_PATH INSTALLER_PATH
+  where ONEYE_PATH is a path to the oneye system folder
+  where INSTALLER_PATH is a path to the oneye installer folder
+EOF
+}
 
-function creator_modules_xml() {
-	local filexml
+#
+# ERRORS
+#
+
+##
+# ERROR
+#
+# Prints the passed error and the help to stderr and exits
+function creator_error() {
+	echo "$1" 1>&2
+	echo 1>&2
+	creator_help 1>&2
+	exit 1
+}
+
+##
+# ERROR: FAILING COMMAND
+#
+# Prints the error "command not found"
+#
+# @param1 Command, which has not been found.
+function creator_error_command() {
+	creator_error "Command not found: $1"
+}
+
+##
+# ERROR: INVALID PARAMETER
+#
+# Prints the error "invalid parameter"
+function creator_error_parameter() {
+	creator_error 'Invalid parameter passed.'
+}
+
+#
+# TOOLS
+#
+
+##
+# BASE64
+#
+# Creates a base64 encoded stream out of a given file.
+#
+# @param1 File to be encoded.
+function creator_base64 {
+	if [[ $(command -v base64) ]]; then
+		base64 --wrap=0 "$1"
+	elif [[ $(command -v openssl) ]]; then
+		openssl base64 "$1" | tr '\r\n' ' ' | sed 's/\s+//g'
+	else
+		creator_error_command 'base64'
+	fi
+}
+
+##
+# MD5
+#
+# Creates the 32 byte long md5 hash out of a given file.
+#
+# @param1 File to be hashed.
+function creator_md5 {
+	local md5=''
+	
+	if [[ $(command -v md5) ]]; then
+		md5=$(md5 -q "$1")
+	elif [[ $(command -v md5sum) ]]; then
+		md5=$(md5sum "$1")
+	else
+		creator_error_command 'md5'
+	fi
+	
+	echo -n ${md5:0:32}
+}
+
+##
+# REALPATH
+#
+# Canonicalizes a given path.
+#
+# @param1 Path to be canonicalized.
+function creator_realpath() {
+	cd "$1"
+	path=$(pwd)
+	cd - > /dev/null
+	echo "$path"
+}
+
+#
+# ACTIONS
+#
+# @param1 ONEYE_PATH
+# @param2 INSTALLER_PATH
+
+##
+# ACTION 1
+#
+# Clean up temporary directory
+function creator_action_1() {
+	echo "Clean up temporary directories"
+	rm -f -r "$1/eyeOS/tmp/*"
+	touch "$1/eyeOS/tmp/.htaccess"
+}
+
+##
+# ACTION 2
+#
+# Copy index.php and settings.php
+function creator_action_2() {
+	echo "Copy index.php and settings.php"
+	cp "$1/index.php" "$2/installer/files/index.txt"
+	cp "$1/settings.php" "$2/installer/files/settings.txt"
+}
+
+##
+# ACTION 3: BASE64
+#
+# Creates a list of all files in a folder and their base64 encoded content.
+#
+# @param1: Path to be scanned.
+# @param2: Path used for substitution.
+function creator_action_3_base64() {
+	local path=''
 	
 	for file in $1/*; do
 		if [[ -d "$file" ]]; then
-			creator_modules_xml "$file" "$2"
+			creator_action_3_base64 "$file" "$2"
 		else
-			filexml="${file:${#2}}"
+			path="${file:${#2}}"
 			
-			echo -n "<$filexml>" | sed 's/&/&amp;/g' >> "$modules_path"
-			cat "$file" | base64 --wrap=0 >> "$modules_path"
-			echo -n "</$filexml>" | sed 's/&/&amp;/g' >> "$modules_path"
+			echo -n -e "\t\t<$path>" | sed 's/&/&amp;/g'
+			creator_base64 "$file"
+			echo "</$path>" | sed 's/&/&amp;/g'
 		fi
 	done
 }
 
-function creator_main () {
-	local i=1
-	local j=6
-	
-	local md5
-	local datetime
-	
-	# Clean up temporary directory
-	echo "$i/$j Clean up temporary directory"
-	echo
-	rm --force --recursive "$oneye_path/$oneyexxxxxxxxxx_dir/tmp/*"
-	touch "$oneye_path/$oneyexxxxxxxxxx_dir/tmp/.htaccess"
-	i=$(( $i + 1 ))
-	
-	# Copy index.php and settings.php
-	echo "$i/$j Copy index.php and settings.php"
-	echo
-	cp "$oneye_path/index.php" "$files_path/index.txt"
-	cp "$oneye_path/settings.php" "$files_path/settings.txt"
-	i=$(( $i + 1 ))
-	
-	# Generate modules.xml
-	echo "$i/$j Generate modules.xml"
-	echo
-	echo -n '<modules><browser>' > "$modules_path"
-	creator_modules_xml "$oneye_path/$browser_dir" "$oneye_path/$browser_dir/"
-	echo -n '</browser><iphone>' >> "$modules_path"
-	creator_modules_xml "$oneye_path/$iphone_dir" "$oneye_path/$iphone_dir/"
-	echo -n '</iphone><mobile>' >> "$modules_path"
-	creator_modules_xml "$oneye_path/$mobile_dir" "$oneye_path/$mobile_dir/"
-	echo -n '</mobile></modules>' >> "$modules_path"
-	i=$(( $i + 1 ))
-	
-	# Create package.eyepackage
-	echo "$i/$j Create package.eyepackage"
-	echo
-	cd "$oneye_path"
-	tar --create --file="$installer_path/package.eyepackage" --gzip --exclude-vcs "$oneyexxxxxxxxxx_dir"
-	i=$(( $i + 1 ))
-	
-	# Update installer/index.php
-	echo "$i/$j Update installer/index.php"
-	echo
-	md5=$(md5sum "$installer_path/package.eyepackage")
-	md5=${md5:0:32}
-	datetime=$(date +%Y%m%d%H%M%S)
-	cat "$installer_path/installer/index.php" | sed "s/define('INSTALL_MD5','.\{32\}');/define('INSTALL_MD5','$md5');/" | sed "s/define('INSTALL_VERSION','\([0-9]\+.[0-9]\+.[0-9]\+.[0-9]\+\(\|-[0-9]\+\)\)\(\|preview[0-9]\+\)');/define('INSTALL_VERSION','\1preview$datetime');/" > "$installer_path/installer/index.php"
-	i=$(( $i + 1 ))
-	
-	# Ready.
-	echo "$i/$j Remember to update \"update.xml\" manually!"
+##
+# ACTION 3: INIT
+#
+# Initializes the third action
+function creator_action_3_init() {
+	echo '<modules>'
+		echo -e '\t<browser>'
+			creator_action_3_base64 "$1/browser" "$1/browser/"
+		echo -e '\t</browser>'
+		echo -e '\t<iphone>'
+			creator_action_3_base64 "$1/iphone" "$1/iphone/"
+		echo -e '\t</iphone>'
+		echo -e '\t<mobile>'
+			creator_action_3_base64 "$1/mobile" "$1/mobile/"
+		echo -e '\t</mobile>'
+	echo -e '</modules>'
 }
 
-creator_main
-cd "$cd"
+##
+# ACTION 3
+#
+# Generate modules.xml
+function creator_action_3() {
+	local modules_path="$installer_path/installer/files/modules.xml"
+	
+	echo "Generate modules.xml"
+	creator_action_3_init "$@" > "$modules_path"
+}
+
+##
+# ACTION 4
+#
+# Create package.eyepackage
+function creator_action_4() {
+	echo "Create package.eyepackage"
+	cd "$1"
+	tar --create --file="$2/package.eyepackage" --gzip --exclude '.git*' "./eyeOS"
+	cd - > /dev/null
+}
+
+##
+# ACTION 5
+#
+# Update installer/index.php
+function creator_action_5() {
+	local md5=$(creator_md5 "$2/package.eyepackage")
+	if [[ $? -ne 0 ]]; then
+		creator_error_command 'md5'
+	fi
+	local md5_from="define('INSTALL_MD5',\s*'.\{0,32\}');"
+	local md5_to="define('INSTALL_MD5', '$md5');"
+	
+	local version=$(date +%Y%m%d%H%M%S)
+	local version_from="define('INSTALL_VERSION',\s*'\([0-9]\+.[0-9]\+.[0-9]\+.[0-9]\+\(\|-[0-9]\+\)\)\(\|preview[0-9]\+\)');"
+	local version_to="define('INSTALL_VERSION', '\1preview$version');"
+	
+	echo "Update installer/index.php"
+	cat "$2/installer/index.php" | sed "s/$md5_from/$md5_to/" | sed "s/$version_from/$version_to/" > "$2/installer/index.php"
+}
+
+#
+# MAIN
+#
+
+##
+# MAIN
+#
+# Main function, handling working directory and calling actions.
+function creator_main() {
+	local i=0
+	local j=5
+	
+	oneye_path=$(creator_realpath "$1")
+	installer_path=$(creator_realpath "$2")
+	
+	cd $(dirname "$0")
+	
+	while [[ $i -lt $j ]]; do
+		i=$(($i + 1))
+		
+		if [[ $i -gt 1 ]]; then
+			echo
+		fi
+		echo -n "$i/$j "
+		"creator_action_$i" "$oneye_path" "$installer_path"
+	done
+	
+	cd - > /dev/null
+}
+
+#
+# PARAMETERS
+#
+
+# Help
+if [[ $# -eq 0 ]] || ([[ $# -eq 1 ]] && ([[ "$1" == '-h' ]] || [[ "$1" == '--help' ]])); then
+	creator_help
+	exit 0
+fi
+
+# Main
+if [[ $# -eq 2 ]]; then
+	creator_main "$@"
+	exit 0
+fi
+
+# Invalid parameter
+creator_error_parameter
