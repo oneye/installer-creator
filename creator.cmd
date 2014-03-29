509f0484bc4c379995d43344965c9111791cf842
Lars Knickrehm <mail@lars-sh.de>
2014-03-29 18:37:27 +0100

    Updated cmd script for latest Cygwin (64bit)

--- creator.cmd
+++ creator.cmd
@@ -8,7 +8,7 @@
 @echo off
 
 rem Path to cygwin directory
-set cygwin_path=C:\cygwin
+set cygwin_path=C:\cygwin64
 if not exist "%cygwin_path%\bin\bash.exe" (
 	goto notFound
 )
@@ -17,22 +17,49 @@ rem Set path variables
 set file=%~n0
 set path=%~dp0
 
-rem Replace back slashes by slashes and remove colons.
+rem Lower case drive letter, replace back slashes by slashes and remove colons.
+set drive=%path:~0,1%
+SET drive=%drive:A=a%
+SET drive=%drive:B=b%
+SET drive=%drive:C=c%
+SET drive=%drive:D=d%
+SET drive=%drive:E=e%
+SET drive=%drive:F=f%
+SET drive=%drive:G=g%
+SET drive=%drive:H=h%
+SET drive=%drive:I=i%
+SET drive=%drive:J=j%
+SET drive=%drive:K=k%
+SET drive=%drive:L=l%
+SET drive=%drive:M=m%
+SET drive=%drive:n=n%
+SET drive=%drive:O=o%
+SET drive=%drive:P=p%
+SET drive=%drive:Q=q%
+SET drive=%drive:R=r%
+SET drive=%drive:S=s%
+SET drive=%drive:T=t%
+SET drive=%drive:U=u%
+SET drive=%drive:V=v%
+SET drive=%drive:W=w%
+SET drive=%drive:X=x%
+SET drive=%drive:Y=y%
+SET drive=%drive:Z=z%
 set unix_path=%path:\=/%
-set unix_path=%unix_path::=%
-
-rem Show the installer creator help.
-echo %path:~0,-1%>"%file%.sh"  --help
-bash --login -i -- "/cygdrive/%unix_path%/%file%.sh" --help
-echo.
-
-rem Read arguments.
-set /P args="%path:~0,-1%>"%file%.sh" "
-echo.
+set unix_path=%drive%%unix_path:~2%
 
 rem Remember current working directory in order to revert it correctly.
 set old_cd=%CD%
 cd /d "%cygwin_path%\bin"
+
+rem Show the installer creator help.
+echo %path:~0,-1%>"%file%.sh" --help
+bash --login -i -- "/cygdrive/%unix_path%/%file%.sh" --help
+echo.
+
+rem Read arguments.
+set /P args="%path:~0,-1%>%file%.sh "
+echo.
 
 rem Pass through all given arguments.
 bash --login -i -- "/cygdrive/%unix_path%/%file%.sh" %args%
